944f60d069a39ca410f4745598eba55df699ca96
Basim Rahmat-Alla <basim.rahmat-alla@kadaster.nl>
2017-08-10 15:04:07 +0200

    pdok-1263 jgit-oplossing exprimenteel versie

--- src/main/java/nl/pdok/catalog/job/RetriveJobEntries.java
+++ src/main/java/nl/pdok/catalog/job/RetriveJobEntries.java
@@ -1,6 +1,7 @@
 package nl.pdok.catalog.job;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,7 +11,41 @@ import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.errors.UnsupportedCredentialItem;
+import org.eclipse.jgit.transport.CredentialItem;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
 public class RetriveJobEntries {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		try {
+			
+			CredentialsProvider.setDefault(new UsernamePasswordCredentialsProvider("zz_pdok", "aa9b4f0b4435cac2f20e1265fa0fa241742f0893"));
+
+			Git git = Git.cloneRepository()
+					  .setURI( "http://github.so.kadaster.nl/PDOK/catalogus" )
+					  .setDirectory( new File("D:/tempDir/pdok-1263-catalogus/") )
+					  .setBranch( "pdok-1263" )
+					  .call();
+			System.out.println(git.log().toString());
+		} catch (InvalidRemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 	public static String retrieveByTypeDataset(String datasetType) throws IOException {
 
@@ -27,7 +62,8 @@ public class RetriveJobEntries {
 		HttpURLConnection httpConnection = (HttpURLConnection) gitURL.openConnection();
 		httpConnection.setRequestProperty("Authorization",
 				"Basic enpfcGRvazphYTliNGYwYjQ0MzVjYWMyZjIwZTEyNjVmYTBmYTI0MTc0MmYwODkz");
-//		Map<String, List<String>> headerMap = httpConnection.getHeaderFields();
+		// Map<String, List<String>> headerMap =
+		// httpConnection.getHeaderFields();
 
 		InputStream fileInputStream = httpConnection.getInputStream();
 		return convertStreamToString(fileInputStream);
